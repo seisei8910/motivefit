b6bcac0f039730124b7c64ea8d4e8187722c0364
SeiseiUtaka <185202718+seisei8910@users.noreply.github.com>
2024-12-18 22:32:53 +0000

    [Finish] 投稿機能の詳細画面作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,11 +10,18 @@ class PostsController < ApplicationController
     redirect_to posts_path
   end
 
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to posts_path
+  end
+
   def index
     @posts = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def edit
