955e37ccebfa10f4483cbc3358c5cc7f8d3c4ad3
SeiseiUtaka <185202718+seisei8910@users.noreply.github.com>
2024-12-21 00:34:44 +0000

    [Finish] 画像サイズ変更の為、ImageMagickをインストール

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,11 +2,11 @@ class Post < ApplicationRecord
   has_one_attached :image
   belongs_to :user
 
-  def get_image
+  def get_image(width, height)
     unless image.attached?
-      file_path = Rails.root.join('app/assets/images/no_image.png')
+      file_path = Rails.root.join('app/assets/images/photo_no-image.png')
       image.attach(io: File.open(file_path), filename: 'default-image.png', content_type: 'image/png')
     end
-    image
+    image.variant(resize_to_limit: [width, height]).processed
   end
 end
