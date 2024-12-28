450a90e966e07f735fd9e917d08ca2657145d8de
SeiseiUtaka <185202718+seisei8910@users.noreply.github.com>
2024-12-28 08:36:12 +0000

    [Add] 画像投稿の任意機能やレイアウト

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,10 +4,7 @@ class Post < ApplicationRecord
   belongs_to :user
 
   def get_image(width, height)
-    unless image.attached?
-      file_path = Rails.root.join('app/assets/images/photo_no-image.png')
-      image.attach(io: File.open(file_path), filename: 'default-image.png', content_type: 'image/png')
-    end
+    return nil unless image.attached?
     image.variant(resize_to_limit: [width, height]).processed
   end
 end
