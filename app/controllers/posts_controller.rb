6291fec657339d5e5a1ee62f9947fb1613277aee
SeiseiUtaka <185202718+seisei8910@users.noreply.github.com>
2025-01-02 07:19:39 +0000

    [Finish] 他ユーザーからのアクセス制限

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
+
   def new
     @post = Post.new
   end
@@ -46,5 +48,12 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:fitness_date, :menu, :body, :image)
+  end
+
+  def is_matching_login_user
+    post = Post.find(params[:id])
+    unless post.user.id == current_user.id
+      redirect_to posts_path
+    end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
+
   def mypage
     @user = User.find(current_user.id)
     @post = Post.new
@@ -28,5 +30,12 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :profile_image, :status_message)
+  end
+
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to posts_path
+    end
   end
 end
