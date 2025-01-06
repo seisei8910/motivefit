83b2029fdd746e314605cfa2c269f8870251be4d
SeiseiUtaka <185202718+seisei8910@users.noreply.github.com>
2025-01-06 21:46:56 +0000

    [Add] 検索機能にアクセスする為のRoutingの設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get "/about", to: "homes#about"
   resources :posts
-  get "/mypage", to: "users#mypage", as: "mypage"
+  get "/mypage", to: "users#mypage"
   resources :users, only: [:show, :edit, :update, :destroy]
+  get "/search", to: "searches#search"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
