9900a962263a389ed33a30edd7734687727e150c
SeiseiUtaka <185202718+seisei8910@users.noreply.github.com>
2025-01-08 07:15:01 +0000

    [Add] フラッシュメッセージを追加

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -12,8 +12,18 @@ class SearchesController < ApplicationController
     # 選択したモデルに応じて検索を実行
     if @model  == "user"
       @users = User.search_for(@word, @method)
+      if @users.count > 0
+        flash.now[:notice] = "新しい仲間との出会いがここに！どんなトレーニングをしているか見てみましょう💪"
+      else
+        flash.now[:alert] = "該当するユーザーが見つかりませんでした💡 条件を変更してみてください！"
+      end
     else
       @posts = Post.search_for(@word, @method)
+      if @posts.count > 0
+        flash.now[:notice] = "みんなのトレーニング記録を発見！インスピレーションを受け取ろう🔥！"
+      else
+        flash.now[:alert] = "まだ誰もそのトピックについて投稿していないようです。ぜひあなたが最初の投稿を🚀！"
+      end
     end
   end
 end
