dd4a748b3214c00e77cb3843dc31587f365f657c
SeiseiUtaka <185202718+seisei8910@users.noreply.github.com>
2025-01-12 12:57:21 +0000

    [Add] application_controller.rbの記述を移す

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,7 +1,16 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
+  before_action :configure_permitted_parameters, if: :devise_controller?
   # before_action :configure_sign_in_params, only: [:create]
+
+  def after_sign_in_path_for(resource)
+    mypage_path
+  end
+
+  def after_sign_out_path_for(resource)
+    about_path
+  end
 
   # GET /resource/sign_in
   # def new
@@ -24,4 +33,10 @@ class Public::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
