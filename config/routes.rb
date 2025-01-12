a13b032d75aaf497870fade3a82b867b40c196df
SeiseiUtaka <185202718+seisei8910@users.noreply.github.com>
2025-01-12 02:45:43 +0000

    [Add] ルーティングの設定、indexビューの作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,25 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :password], controllers: {
     sessions: 'admin/sessions'
   }
+
+  namespace :admin do
+    get 'dashboards', to: 'dashboards#index'
+  end
+
   devise_for :users
+
   root to: "homes#top"
+
   get "/about", to: "homes#about"
+
   resources :posts do
     resources :post_comments, only: [:create, :destroy]
   end
+
   get "/mypage", to: "users#mypage"
+
   resources :users, only: [:show, :edit, :update, :destroy]
+
   get "/search", to: "searches#search"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
