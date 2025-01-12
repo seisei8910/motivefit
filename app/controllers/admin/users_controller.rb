ccebeabd65b3fda8304be49359ab57449917e240
SeiseiUtaka <185202718+seisei8910@users.noreply.github.com>
2025-01-12 14:25:21 +0000

    [Add] admin/usersコントローラーにshowアクションを追加、ルーティングを更新

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,9 @@
 class Admin::UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    @posts = @user.posts.order(created_at: :desc)
+  end
+
   def destroy
     @user = User.find(params[:id])
     @user.destroy
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
     get 'dashboards', to: 'dashboards#index'
-    resources :users, only: [:destroy]
+    resources :users, only: [:show, :destroy]
   end
 
   scope module: :public do
