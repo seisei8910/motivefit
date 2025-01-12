059b45acf56c06f76fb3faa25ea0a5cfc78e47b2
SeiseiUtaka <185202718+seisei8910@users.noreply.github.com>
2025-01-12 01:46:59 +0000

    [Add] 管理者用ユーザーの作成、ログイン、ログアウト後の遷移先を設定

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -24,4 +24,14 @@ class Admin::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+  protected
+
+  def after_sign_in_path_for(resource)
+    admin_dashboards_path # ログイン後にリダイレクトするパス
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    new_admin_session_path # ログアウト後にリダイレクトするパス
+  end
 end
