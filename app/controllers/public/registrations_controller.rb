2678905d58ced44753c0a80d3709d87e51fe6746
SeiseiUtaka <185202718+seisei8910@users.noreply.github.com>
2025-01-14 10:24:07 +0000

    [Fix] Public/Registrationsコントローラー名前が入力できるように修正

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -1,8 +1,17 @@
 # frozen_string_literal: true
 
 class Public::RegistrationsController < Devise::RegistrationsController
+  before_action :configure_permitted_parameters, if: :devise_controller?
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
+
+  def after_sign_in_path_for(resource)
+    mypage_path
+  end
+
+  def after_sign_out_path_for(resource)
+    about_path
+  end
 
   # GET /resource/sign_up
   # def new
