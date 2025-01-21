ffb8bcd542833ccb371eb4acafd311c1ec0d63e4
SeiseiUtaka <185202718+seisei8910@users.noreply.github.com>
2025-01-21 22:41:05 +0000

    [Add] フォロー機能に一意性制約を追加

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -3,4 +3,6 @@ class Relationship < ApplicationRecord
   belongs_to :follower, class_name: "User"
   belongs_to :followed, class_name: "User"
 
+  validates :follower_id, uniqueness: { scope: :followed_id, message: "この組み合わせはすでに存在します" }
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_01_19_065020) do
+ActiveRecord::Schema.define(version: 2025_01_21_222336) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -81,6 +81,7 @@ ActiveRecord::Schema.define(version: 2025_01_19_065020) do
     t.integer "followed_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
   end
 
   create_table "users", force: :cascade do |t|
