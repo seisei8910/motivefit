1f1e07924d225c8796f905cb0475fad6e604361b
SeiseiUtaka <185202718+seisei8910@users.noreply.github.com>
2025-01-25 02:15:14 +0000

    [Add] roomコントローラー編集、rooms/index,showビュー作成

--- app/controllers/public/rooms_controller.rb
+++ app/controllers/public/rooms_controller.rb
@@ -1,2 +1,30 @@
 class Public::RoomsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @room = Room.create
+    @current_participant = @room.participants.create(user_id: current_user.id)
+    @another_participant = @room.participants.create(user_id: params[:participant][:user_id])
+    redirect_to room_path(@room)
+  end
+
+  def index
+    my_room_id = current_user.participants.pluck(:room_id)
+    @another_participants = Participant
+                        .where(room_id: my_room_id)
+                        .where.not(user_id: current_user.id)
+                        .preload(room: :messages).preload(user: { icon_attachment: :blob })
+  end
+
+  def show
+    @room = Room.find(params[:id])
+    if @room.participants.where(user_id: current_user.id).present?
+      @messages = @room.messages.all
+      @message = Message.new
+      @entries = @room.entries
+      @another_participant = @participants.where.not(user_id: current_user.id).first
+    else
+      redirect_back(fallback_location: root_path)
+    end
+  end
 end
