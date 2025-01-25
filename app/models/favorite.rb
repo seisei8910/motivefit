e349b4c571d86b6e7519c829e8b603ef969d8eae
SeiseiUtaka <185202718+seisei8910@users.noreply.github.com>
2025-01-25 07:56:45 +0000

    [Add] モデルの関連付け実装

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -3,6 +3,8 @@ class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
+  has_one :notification, as: :notifiable, dependent: :destroy
+
   validates :user_id, uniqueness: {scope: :post_id}
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,7 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :notifications, as: :notifiable, dependent: :destroy
 
   validates :fitness_date, presence: true
   validates :menu, presence: true
