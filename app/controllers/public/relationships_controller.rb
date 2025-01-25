ea5f736f0bfa4eef2104d8a0d867638fefd19ce7
SeiseiUtaka <185202718+seisei8910@users.noreply.github.com>
2025-01-25 04:30:11 +0000

    [Add] relationshipsコントローラーにDM機能の記述を追加

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -17,12 +17,44 @@ class Public::RelationshipsController < ApplicationController
     @post = Post.new
     @user = User.find(params[:user_id])
     @users = @user.followings
+    @current_participant = Participant.where(user_id: current_user.id)
+    @another_participant = Participant.where(user_id: @user.id)
+    unless @user.id == current_user.id
+      @current_participant.each do |current|
+        @another_participant.each do |another|
+          if current.room_id == another.room_id
+            @is_room = true
+            @room_id = current.room_id
+          end
+        end
+      end
+      unless @is_room
+        @room = Room.new
+        @participant = Participant.new
+      end
+    end
   end
 
   def followers
     @post = Post.new
     @user = User.find(params[:user_id])
     @users = @user.followers
+    @current_participant = Participant.where(user_id: current_user.id)
+    @another_participant = Participant.where(user_id: @user.id)
+    unless @user.id == current_user.id
+      @current_participant.each do |current|
+        @another_participant.each do |another|
+          if current.room_id == another.room_id
+            @is_room = true
+            @room_id = current.room_id
+          end
+        end
+      end
+      unless @is_room
+        @room = Room.new
+        @participant = Participant.new
+      end
+    end
   end
 
 end
