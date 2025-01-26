a95c707f054cfac1281b9c57ae07e4d8024555b3
SeiseiUtaka <185202718+seisei8910@users.noreply.github.com>
2025-01-26 05:53:17 +0000

    [Add] 通知コントローラーの編集

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,2 +1,15 @@
 class NotificationsController < ApplicationController
+
+  def update
+    notification = current_user.notifications.find(params[:id])
+    notification.update(read: true)
+    # 通知の種類によるリダイレクトパスの生成
+    case notification.notifiable_type
+    when "Post"
+      redirect_to post_path(notification.notifiable)
+    else
+      redirect_to user_path(notification.notifiable.user)
+    end
+  end
+
 end
