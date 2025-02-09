f72a1d264aaf0a4d36497ddce4dc32f2ebea25fa
SeiseiUtaka <185202718+seisei8910@users.noreply.github.com>
2025-02-09 13:24:08 +0000

    [Add] auto_scroll.jsのファイルを作成し記述を整備

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,20 +12,8 @@ import "jquery";
 import "popper.js";
 import "bootstrap";
 import "../stylesheets/application";
+import "./auto_scroll";
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
-
-document.addEventListener("turbolinks:load", function() {
-// ↪︎ windowがロードされた時にアクションを実行するように設定
-  if (document.getElementById("area")) {
-    // ↪︎ areaのIDがある場合に処理を実行させる（これがないとチャット画面がなくても常にJavaScriptが動いてしまいます）
-    var scrollPosition = document.getElementById("area").scrollTop;
-    // ↪︎ area要素のスクロールされた時の最も高い場所を取得
-    var scrollHeight = document.getElementById("area").scrollHeight;
-    // ↪︎ area要素自体の最も高い場所を取得
-    document.getElementById("area").scrollTop = scrollHeight;
-    // ↪︎ area要素のスクロールされた時の最も高い場所をarea要素自体の最も高い場所として指定してあげる
-  }
-});
