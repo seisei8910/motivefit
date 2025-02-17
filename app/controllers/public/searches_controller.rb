db0a890d895cdb037771da025905f9598c5b36d0
SeiseiUtaka <185202718+seisei8910@users.noreply.github.com>
2025-02-17 13:54:29 +0000

    [fix] searchesコントローラーの記述変更

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -14,31 +14,30 @@ class Public::SearchesController < ApplicationController
       else
         flash.now[:alert] = "該当するユーザーが見つかりませんでした💡 条件を変更してみてください！"
       end
+      @current_participant = Participant.where(user_id: current_user.id)
+      @users.each do |user|
+        @another_participant = Participant.where(user_id: user.id)
+        unless user.id == current_user.id
+          @current_participant.each do |current|
+            @another_participant.each do |another|
+              if current.room_id == another.room_id
+                @is_room = true
+                @room_id = current.room_id
+              end
+            end
+          end
+          unless @is_room
+            @room = Room.new
+            @participant = Participant.new
+          end
+        end
+      end
     else
       @posts = Post.search_for(@word, @method).order(created_at: :desc).page(params[:page])
       if @posts.count > 0
         flash.now[:notice] = "みんなのトレーニング記録を発見！インスピレーションを受け取ろう🔥！"
       else
         flash.now[:alert] = "まだ誰もそのトピックについて投稿していないようです。ぜひあなたが最初の投稿を🚀！"
-      end
-    end
-
-    @current_participant = Participant.where(user_id: current_user.id)
-    @users.each do |user|
-      @another_participant = Participant.where(user_id: user.id)
-      unless user.id == current_user.id
-        @current_participant.each do |current|
-          @another_participant.each do |another|
-            if current.room_id == another.room_id
-              @is_room = true
-              @room_id = current.room_id
-            end
-          end
-        end
-        unless @is_room
-          @room = Room.new
-          @participant = Participant.new
-        end
       end
     end
   end
