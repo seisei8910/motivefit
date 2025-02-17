85a4ed1f8719d88f03d73a53c1f340cb4dcdac4a
SeiseiUtaka <185202718+seisei8910@users.noreply.github.com>
2025-02-17 13:49:33 +0000

    [Add] ユーザー検索結果画面にチャットボタンを表示

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -22,5 +22,24 @@ class Public::SearchesController < ApplicationController
         flash.now[:alert] = "まだ誰もそのトピックについて投稿していないようです。ぜひあなたが最初の投稿を🚀！"
       end
     end
+
+    @current_participant = Participant.where(user_id: current_user.id)
+    @users.each do |user|
+      @another_participant = Participant.where(user_id: user.id)
+      unless user.id == current_user.id
+        @current_participant.each do |current|
+          @another_participant.each do |another|
+            if current.room_id == another.room_id
+              @is_room = true
+              @room_id = current.room_id
+            end
+          end
+        end
+        unless @is_room
+          @room = Room.new
+          @participant = Participant.new
+        end
+      end
+    end
   end
 end
